95e090160732963cd6d7a87d4236d8911ce9348c
T Brichetto <TaraPersonal@MacBook-Air.local>
2020-09-25 23:18:13 -0700

    Questions array and initial functions

--- index.js
+++ index.js
@@ -1,6 +1,128 @@
+// Const
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project? (Required)",
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a title for your project.');
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name? (Required)",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your name.');
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a brief description of your project. (Required)",
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please include a project description.');
+                return false;
+            }
+        }
+    },
+    {
+        type: "checkbox",
+        name: "languages",
+        message: "What languages did you use to create your project? (Optional) Please check all that apply.",
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the installation instructions for users wishing to view/use your project? (Required)",
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please include installation instructions.');
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What are the instructions for use? (Required) Include examples as necessary.",
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please include usage instructions.');
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Please list the names of any additional project contributors. (Optional)",
+    },
+    {
+        type: "input",
+        name: "future-contributions",
+        message: "Please include guidelines for future contributors to follow. (Optional)",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please include any tests for your application and examples on how to run them. (Optional)"
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Please check any licenses you wish to include in your ReadMe file. (Optional)",
+        choices: ['Apache', 'Boost', 'BSD', 'Creative Commons', 'Eclipse', 'GNU', 'IBM', 'ISC', 'MIT', 'Mozilla', 'Open Data Commons', 'Perl', 'SIL', 'Unlicense', 'WTFPL', 'Zlib']
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your Github username? (Required)",
+        validate: usernameInput => {
+            if (usernameInput) {
+                return true;
+            } else {
+                console.log('Please enter your Github username.');
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address? (Required)",
+        validate: emailInput => {
+            if(emailInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address.');
+                return false;
+            }
+        }
+    }
 ];
 
 // function to write README file
@@ -9,8 +131,12 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
-}
+    inquirer.prompt(questions)
+    .then(answers => {
+        console.log(answers);
+        writeToFile("./generated-readme/README.md", generateMarkdown({...answers})
+    )});
+};
 
 // function call to initialize program
 init();
