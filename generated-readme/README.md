f82105892b09b0853eb00f405bf90a9dfed04448
T Brichetto <TaraPersonal@MacBook-Air.local>
2020-09-26 22:15:59 -0700

    ReadMe formatting and license image

--- generated-readme/README.md
+++ generated-readme/README.md
@@ -1,16 +1,25 @@
-# g
-## Project Description g
-## Created by g
+# Project Awesome
+![GitHub license](https://img.shields.io/badge/license-Apache-blue.svg)
+## Creator
+Tara
 ## Table of Contents
-[Text](#Section)
-## Description g
-## Languages Used JavaScript
-## Installation g
-## Usage g
-## Additional Contributors g
-## Future Contributions g
-## Tests g
-## Licenses ![GitHub license](https://img.shields.io/badge/license-Apache-blue.svg)
+[Questions](#Questions)
+## Project Description
+It's pretty cool
+## Languages Used
+JavaScript,HTML,CSS
+## Installation
+Install it
+## Usage
+Use it
+## Additional Contributors
+JoeBob
+## Future Contributions
+Contribute to it
+## Tests
+Test
+## Licenses
+Apache
 ## Questions
-See g's other projects at https://github.com/g
-Please contact g with any questions at g
+See Tara's other projects at https://github.com/stephen
+Please contact Tara with any questions at a@a.com
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,18 +6,27 @@ const fs = require('fs');
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-## Project Description ${data.description}
-## Created by ${data.name}
+![GitHub license](https://img.shields.io/badge/license-${data.license}-blue.svg)
+## Creator
+${data.name}
 ## Table of Contents
-[Text](#Section)
-## Description ${data.description}
-## Languages Used ${data.languages}
-## Installation ${data.installation}
-## Usage ${data.usage}
-## Additional Contributors ${data.contributors}
-## Future Contributions ${data.future}
-## Tests ${data.test}
-## Licenses ![GitHub license](https://img.shields.io/badge/license-${data.license}-blue.svg)
+[Questions](#Questions)
+## Project Description
+${data.description}
+## Languages Used
+${data.languages}
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## Additional Contributors
+${data.contributors}
+## Future Contributions
+${data.future}
+## Tests
+${data.test}
+## Licenses
+${data.license}
 ## Questions
 See ${data.name}'s other projects at https://github.com/${data.username}
 Please contact ${data.name} with any questions at ${data.email}
